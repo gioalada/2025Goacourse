fb72d5662b18d055a26051b6324046bd9db57e33
gioalada <aallaaddaa24@gmail.com>
2025-01-20 00:00:13 +0400

    commit

---
+++ day02/classwork/day02.py
@@ -0,0 +1,9 @@
+name = "giorgi"
+
+lastname = "aladashvili"
+
+age = 14
+
+age =str(age)
+
+print(name,lastname,age )
--- day04/classwork/day04.py
+++ day04/classwork/day04.py
@@ -1,1 +1,26 @@
-print("no classwork")
+# 4 ცვლადი სადაც ინახავთ string-ებს
+
+string1 = "Hello"
+string2 = "html"
+string3 = "Python"
+string4 = "Programming"
+
+# 4 ცვლადი სადაც ინახავთ integer-ებს
+
+integer1 = 20
+integer2 = 40
+integer3 = 60
+integer4 = 80
+
+# 4 ცვლადი სადაც ინახავთ float-ებს
+
+float1 = 3.54
+float2 = 1.71
+float3 = 0.61
+float4 = 6.99
+
+
+# დაბეჭდვა
+print("String variables:", string1, string2, string3, string4)
+print("Integer variables:", integer1, integer2, integer3, integer4)
+print("Float variables:", float1, float2, float3, float4)
