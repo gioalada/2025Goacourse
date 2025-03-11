a3f3a55240b10cd249449cf6086820e06d1b7109
gioalada <aallaaddaa24@gmail.com>
2025-03-11 18:13:00 +0400

    commit

--- day04/homework/day04.py
+++ day04/homework/day04.py
@@ -4,5 +4,33 @@ father = "ilia"
 sister = "tata"
 grandfather = "nodari"
 
-
 print("My mother is " + mother + ", my father is " + father + ", my brother is " +  + ", my sister is " + sister + ", and my grandfather is " + grandfather + ".")
+
+num1 = 15
+num2 = 5
+num3 = 8
+num4 = 20
+num5 = 66
+print(num1 + num2)
+print(num3 + num4)
+print(num2 + num5)
+print(num1 + num3)
+print(num4 + num5)
+
+print(num1 - num2)
+print(num3 - num4)
+print(num2 - num5)
+print(num1 - num3)
+print(num4 - num5)
+
+print(num1 * num2)
+print(num3 * num4)
+print(num2 * num5)
+print(num1 * num3)
+print(num4 * num5)
+
+print(num1 // num2)
+print(num3 // num4)
+print(num2 // num5)
+print(num1 // num3)
+print(num4 // num5)
