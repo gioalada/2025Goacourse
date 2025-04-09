7248d3107c593d464a31a18a445408908f62076f
gioalada <aallaaddaa24@gmail.com>
2025-04-09 21:52:01 +0400

    commit

---
+++ day14/homework/day14.py
@@ -0,0 +1,31 @@
+sum = 0
+for i in range(1, 11):
+    sum += i
+print("ჯამი:", sum)
+
+for i in range(20):
+    print(i)
+
+for i in range(31):
+    print(i)
+
+for i in range(15, 80):
+    print(i)
+
+
+for i in range(65):
+    print(i)
+
+sum = 0
+for i in range(51):
+    sum += i
+print("ჯამი 50-მდე:", sum)
+
+
+for i in range(0, 100, 2):
+    print(i)
+
+  
+
+
+
